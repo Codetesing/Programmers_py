087e02dacf37d4a6faf2b3aeaaab432fc4361317
Codetesting <37256601+Codetesing@users.noreply.github.com>
2021-11-04 16:18:46 +0900

    올바른 괄호
    올바른 괄호

---
+++ level_2/example11.py
@@ -0,0 +1,20 @@
+def solution(s):
+    answer = True
+    cnt = 0
+
+    for i in s :
+        if cnt < 0 :
+            return False
+        if i == '(' :
+            cnt += 1
+        elif i == ')' :
+            cnt -= 1
+
+    if cnt != 0 :
+        return False
+
+    return True
+
+s = "(()("
+
+print(solution(s))
